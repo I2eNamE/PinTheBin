25d6e10d2b418883376aa64dbe6695a0601ecda6
Phutawan Chanrueng <66838025+ATOMIC09@users.noreply.github.com>
2023-12-15 02:51:16 +0700

    Back to localhost

--- Frontend/app/editbin/[id]/page.js
+++ Frontend/app/editbin/[id]/page.js
@@ -25,7 +25,7 @@ export default function EditBin({ params }) {
   const [isBinNotFoundVisible, setIsBinNotFoundVisible] = useState(false);
 
 
-  let url = 'http://pinthebin-backend.vercel.app/';
+  let url = 'http://localhost:8080/';
   
 
 
