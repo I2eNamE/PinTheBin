cdf2193ecd29f4c05614b0e26444cbec9a8d12e7
Phutawan Chanrueng <66838025+ATOMIC09@users.noreply.github.com>
2023-12-15 16:02:25 +0700

    Change domain

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const mysql = require('mysql');
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 
@@ -17,7 +17,7 @@ const port = 4000
 const secretKey = process.env.secretKey;
 
 
-app.use(cors({origin: true}));
+// app.use(cors({origin: true}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 //  wan to check jwt token before use other function except  /login
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://pinthebin.vercel.app/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "pinthebin.vercel.app"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     if (!(req.path === "/login" || req.path === "/register" || req.path === "/upload" || req.path === "/test" || req.path === "/user")) {
         const result = verifyToken(req, res, next);
